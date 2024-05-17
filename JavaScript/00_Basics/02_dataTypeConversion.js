9b3fe4339bfb56128a95d9b173bd9b7cebfd2db6
SrjAdhikari <surajadhikari055@gmail.com>
2024-05-17 09:50:28 +0900

    Learn about datatype conversion

--- JavaScript/00_Basics/02_dataTypeConversion.js
+++ JavaScript/00_Basics/02_dataTypeConversion.js
@@ -4,37 +4,49 @@
     console.log(typeof (score));
 */
 
-//* ****************************************************
-    let value = "100";          // String variable
-    console.log(typeof value);  // Output -> string
+//* ******************************************************************
+    let value = "100";                          // String variable
+    console.log(typeof value);                  // Output -> string
 
-    // Convert from string to number 
-    let valueInNumber = Number(value);
-    console.log(typeof valueInNumber);  // Output -> number
+    let valueInNumber = Number(value);          // Conversation from string to number 
+    console.log(typeof valueInNumber);          // Output -> number
 
-//* ****************************************************
-    let stringValue = "100ABC";         // String variable
-    console.log(typeof stringValue);    // Output -> string
+//* ******************************************************************
+    let stringValue = "100ABC";                 // String variable
+    console.log(typeof stringValue);            // Output -> string
 
-    // Convert from string to number 
-    let valueInNumber1 = Number(stringValue);
-    console.log(typeof valueInNumber1);  // Output -> number
-    console.log(valueInNumber1);         // Output -> NaN
+    let valueInNumber1 = Number(stringValue);   // Conversation from string to number 
+    console.log(typeof valueInNumber1);         // Output -> number
+    console.log(valueInNumber1);                // Output -> NaN
 
-//* ****************************************************
-    let nullValue = null;
-    console.log(typeof nullValue);    // Output -> object
+//* ******************************************************************
+    let nullValue = null;                       // Null variable
+    console.log(typeof nullValue);              // Output -> object
 
-    // Convert from string to number 
-    let nullInNumber = Number(nullValue);
-    console.log(typeof nullInNumber);   // Output -> number
-    console.log(nullInNumber);          // Output -> 0
+    let nullInNumber = Number(nullValue);       // Conversation from null to number 
+    console.log(typeof nullInNumber);           // Output -> number
+    console.log(nullInNumber);                  // Output -> 0
 
-//* ****************************************************
-    let undefineValue = undefined;
-    console.log(typeof undefineValue);    // Output -> undefined
+//* ******************************************************************
+    let undefineValue = undefined;                  // Undefined variable
+    console.log(typeof undefineValue);              // Output -> undefined
 
-    // Convert from string to number 
-    let undefineInNumber = Number(undefineValue);
-    console.log(typeof undefineInNumber);   // Output -> number
-    console.log(undefineInNumber);          // Output -> NaN
+    let undefineInNumber = Number(undefineValue);   // Conversation from undefined to number 
+    console.log(typeof undefineInNumber);           // Output -> number
+    console.log(undefineInNumber);                  // Output -> NaN
+
+//* ******************************************************************
+    let isLoggedIn = true;                       // Boolean variable
+    console.log(typeof isLoggedIn);              // Output -> boolean
+
+    let booleanInNumber = Number(isLoggedIn);   // Conversation from boolean to number 
+    console.log(typeof booleanInNumber);        // Output -> number
+    console.log(booleanInNumber);               // Output -> 1
+
+//* ******************************************************************
+    let num = 55;                           // Number variable
+    console.log(typeof num);                // Output -> number
+
+    let numberInString = String(num);       // Conversation from number to string 
+    console.log(typeof numberInString);     // Output -> string
+    console.log(numberInString);            // Output -> 55
