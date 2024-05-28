b84679916a4bcd0ef3832a836e91f10423e77de3
SrjAdhikari <surajadhikari055@gmail.com>
2024-05-28 23:23:13 +0900

    Project 1 -> Find Model

---
+++ 03_JavaScript/09_DOM_Projects/02_Model/app.js
@@ -0,0 +1,19 @@
+let openBtn = document.getElementById('open-btn');
+
+let modelContainer = document.getElementById('model-container');
+
+let closeBtn = document.getElementById('close-btn');
+
+openBtn.addEventListener('click', function(){
+    modelContainer.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', function(){
+    modelContainer.style.display = 'none';
+});
+
+window.addEventListener('click', function(event){
+    if(event.target === modelContainer){
+        modelContainer.style.display = 'none';
+    }
+});
