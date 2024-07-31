3ae10cbd67404b3e05d3f14d09ca9e4f57275ae1
SrjAdhikari <surajadhikari055@gmail.com>
2024-08-01 01:29:10 +0900

    Add input field validation and update local storage
    - Implemented validation to ensure all input fields are filled.
    - Added functionality to track goal completion and reflect it in the UI.
    - Updated local storage handling to store and retrieve goal data.

--- 04_Projects/Focus_On_Today/app.js
+++ 04_Projects/Focus_On_Today/app.js
@@ -1,10 +1,36 @@
 const checkBoxList = document.querySelectorAll(".custom-checkbox");
 const inputFields = document.querySelectorAll(".goal-input");
 const errorMsg = document.querySelector(".error-msg");
+const progressLabel = document.querySelector('.progress-label');
 const progressBar = document.querySelector(".progress-bar");
 const progressValue  = document.querySelector(".progress-indicator");
 
-let isAllGoalsFilled = false;
+const quotes = [
+    'Raise the bar by completing your goals!',
+    'Well begun is half done!',
+    'Just a step away, keep going!',
+    'Whoa! You just completed all the goals, time for chill 😊',
+];
+
+const allGoals = JSON.parse(localStorage.getItem("allGoals")) || {
+    goal1 : {
+        name : "",
+        completed : false
+    },
+    goal2 : {
+        name : "",
+        completed : false
+    },
+    goal3 : {
+        name : "",
+        completed : false
+    }
+};
+let completedGoalsCount = Object.values(allGoals).filter((goal) => goal.completed).length;
+
+progressValue.style.width = `${(completedGoalsCount / inputFields.length) * 100}%`;
+progressValue.firstElementChild.innerText = `${completedGoalsCount}/${inputFields.length} completed`;
+progressLabel.innerText = quotes[completedGoalsCount];
 
 checkBoxList.forEach((checkbox) => {
     checkbox.addEventListener("click", (e) => {
@@ -14,6 +40,13 @@ checkBoxList.forEach((checkbox) => {
 
         if(isAllGoalsFilled) {
             checkbox.parentElement.classList.toggle("completed");
+            const goalId = checkbox.nextElementSibling.id;
+            allGoals[goalId].completed = !allGoals[goalId].completed;
+            completedGoalsCount = Object.values(allGoals).filter((goal) => goal.completed).length;
+            progressValue.style.width = `${(completedGoalsCount / inputFields.length) * 100}%`;
+            progressValue.firstElementChild.innerText = `${completedGoalsCount}/${inputFields.length} completed`;
+            progressLabel.innerText = quotes[completedGoalsCount];
+            localStorage.setItem("allGoals", JSON.stringify(allGoals));
         }
         else {
             progressBar.classList.add("show-error");
@@ -23,7 +56,28 @@ checkBoxList.forEach((checkbox) => {
 });
 
 inputFields.forEach((input) => {
+    if (allGoals[input.id]) {
+        input.value = allGoals[input.id].name;
+
+        if(allGoals[input.id].completed){
+            input.parentElement.classList.add("completed");
+        }    
+    } 
+    else {
+        input.value = '';
+    }
+
     input.addEventListener("focus", () => {
         progressBar.classList.remove("show-error");
+    });
+
+    input.addEventListener("input", (e) => {
+        if(allGoals[input.id].completed) {
+            input.value = allGoals[input.id].name;
+            return;
+        }
+
+        allGoals[input.id].name = input.value
+        localStorage.setItem("allGoals", JSON.stringify(allGoals));
     });
 });
